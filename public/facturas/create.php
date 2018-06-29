0d17986ba871b88468dc9e5df0057462303d5f39
Guido Taján <guidotajan@gmail.com>
2018-06-29 05:01:01 -0300

    La facturación ahora se encarga del control de stock también.

--- public/facturas/create.php
+++ public/facturas/create.php
@@ -6,22 +6,55 @@ checkAuth([1]);
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $result = true;
   $conn = getConnection();
-  $query = $conn->prepare("INSERT INTO facturas (fecha, tipo_factura, cliente_id) VALUES (CURDATE(), :tipo_factura, :cliente_id);");
-  if (!$query->execute([ 'tipo_factura' => $_POST['tipo_factura'], 'cliente_id' => $_POST['cliente_id'] ])) {
-    die();
-  }
-  $factura_id = $conn->lastInsertId();
   $detalles = $_POST['detalles'];
+
+  // Verifica que haya stock para todos los productos solicitados.
   foreach ($detalles as $detalle) {
-    $query = $conn->prepare("INSERT INTO detalles (cantidad, subtotal, producto_id, factura_id) VALUES (:cantidad, :subtotal, :producto_id, :factura_id);");
-    if (!$query->execute([ 'cantidad' => $detalle['cantidad'], 'subtotal' => $detalle['subtotal'], 'producto_id' => $detalle['producto_id'], 'factura_id' => $factura_id ])) {
+    $query = $conn->prepare("SELECT descripcion, stock FROM productos WHERE id = ?");
+    $query->execute([$detalle['producto_id']]);
+    $producto = $query->fetch();
+    if ($producto['stock'] - $detalle['cantidad'] < 0) {
+      // Si no hay stock suficiente se notifica que no se puede realizar la factura.
       $result = false;
+      $error = "El producto '".$producto['descripcion']."' no tiene stock suficiente para realizar la facturación.";
+      break;
+    }
+  }
+  // Si no hay errores crear la factura.
+  if ($result) {
+    $query = $conn->prepare("INSERT INTO facturas (fecha, tipo_factura, cliente_id) VALUES (CURDATE(), :tipo_factura, :cliente_id);");
+    if (!$query->execute([ 'tipo_factura' => $_POST['tipo_factura'], 'cliente_id' => $_POST['cliente_id'] ])) {
+      $result = false;
+      $error = "No se ha podido crear la factura.";
+    } else {
+      $factura_id = $conn->lastInsertId();
+    }
+  }
+  // Si no hubo errores en la creación de la factura, crear los detalles.
+  if (isset($factura_id)) {
+    foreach ($detalles as $detalle) {
+      // Actualizar el stock por cada detalle.
+      $query = $conn->prepare("UPDATE productos SET stock = ((SELECT stock FROM (SELECT * FROM productos) AS p2 WHERE p2.id = productos.id) - :cantidad) WHERE id = :id;");
+      $query->execute([ 'cantidad' => $detalle['cantidad'], 'id' => $detalle['producto_id'] ]);
+      // Crear el registro de detalle.
+      $query = $conn->prepare("INSERT INTO detalles (cantidad, subtotal, producto_id, factura_id) VALUES (:cantidad, :subtotal, :producto_id, :factura_id);");
+      $inputs = [
+        'cantidad' => $detalle['cantidad'],
+        'subtotal' => $detalle['subtotal'],
+        'producto_id' => $detalle['producto_id'],
+        'factura_id' => $factura_id
+      ];
+      if (!$query->execute($inputs)) {
+        // En caso de error, notificar.
+        $result = false;
+        $error = "No se ha podido crear un detalle de la factura.";
+      }
     }
   }
 }
 
-$productos = getConnection()->query("SELECT * FROM productos WHERE estado != 0;")->fetchAll();
-$clientes = getConnection()->query("SELECT * FROM clientes WHERE estado != 0;")->fetchAll();
+$productos = getConnection()->query("SELECT * FROM productos WHERE estado != 0 AND stock > 0 ORDER BY descripcion;")->fetchAll();
+$clientes = getConnection()->query("SELECT * FROM clientes WHERE estado != 0 ORDER BY nombre_apellido;")->fetchAll();
 
 define('PAGE_VIEW', 'facturas_create.php');
 require('../../layout.php');
--- views/facturas_create.php
+++ views/facturas_create.php
@@ -4,7 +4,7 @@ if (isset($result)) {
     <div class="alert alert-success">Se ha creado correctamente la factura.</div>
 <?php
   } else { ?>
-    <div class="alert alert-danger">Ha ocurrido un error al crear la factura.</div>
+    <div class="alert alert-danger"><?php echo isset($error) ? $error : 'Ha ocurrido un error al crear la factura.' ?></div>
 <?php
   }
 }
@@ -52,7 +52,7 @@ if (isset($result)) {
   <div class="input-group">
     <select id="productoId" class="form-control" required>
       <?php foreach ($productos as $producto) { ?>
-        <option value="<?php echo $producto['id'] ?>"><?php echo $producto['descripcion'].' - $'.$producto['precio'] ?></option>
+        <option value="<?php echo $producto['id'] ?>"><?php echo $producto['descripcion'].' - $'.$producto['precio'].' - Stk: '.$producto['stock'] ?></option>
       <?php } ?>
     </select>
     <input type="number" id="cantidad" class="form-control" placeholder="Cantidad" required>
@@ -63,7 +63,7 @@ if (isset($result)) {
 </form>
 <hr>
 <button class="btn btn-block btn-primary" type="submit" form="finalizar">Finalizar</button>
-<script src="../js/facturas_create.js" type="text/babel"></script>
+<script src="../js/facturas_create.js" language="JavaScript" type="text/javascript"></script>
 <div class="modal fade" tabindex="-1" role="dialog" id="errorDetalles">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
