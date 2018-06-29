52ff74ef295b2e8eff584495aacffb0d87f61019
Guido Taján <guidotajan@gmail.com>
2018-06-29 05:12:50 -0300

    Mostrar fechas mientras se esten filtrando los clientes.

--- views/cliente_index.php
+++ views/cliente_index.php
@@ -5,8 +5,8 @@
     <div class="input-group-prepend">
       <span class="input-group-text">Filtrar por fecha</span>
     </div>
-    <input class="form-control" type="date" name="fecha_i" required>
-    <input class="form-control" type="date" name="fecha_f" required>
+    <input class="form-control" type="date" name="fecha_i" value="<?php if (isset($limites)) echo $limites[0] ?>" required>
+    <input class="form-control" type="date" name="fecha_f" value="<?php if (isset($limites)) echo $limites[1] ?>" required>
     <div class="input-group-append">
       <button class="btn btn-secondary" type="submit">Filtrar</button>
       <?php if (isset($limites)) { ?><a class="btn btn-secondary" href="index.php">Reiniciar</a><?php } ?>
